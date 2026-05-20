7a8276ed7afaacb87f78308f52e1a77e2cc8a795
Yahya9102 <yaya_hussein@hotmail.com>
2026-05-20 10:33:10 +0200

    made app more 12 factor app like

--- src/main/java/systementor/cloudstoreuserservice/config/SecurityConfig.java
+++ src/main/java/systementor/cloudstoreuserservice/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package systementor.cloudstoreuserservice.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
